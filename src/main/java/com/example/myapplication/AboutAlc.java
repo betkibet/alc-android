d087860ac8adcd54c1cfe7dbe16d6daaa2282615
betkibet <kibetnamanga@gmail.com>
2019-07-17 10:27:16 +0300

    upload files

---
+++ src/main/java/com/example/myapplication/AboutAlc.java
@@ -0,0 +1,28 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.webkit.WebView;
+
+public class AboutAlc extends AppCompatActivity {
+
+    private WebView mWebview;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_alc);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        mWebview = (WebView)findViewById(R.id.webview);
+        mWebview.loadUrl("https://andela.com/alc/");
+
+
+    }
+
+}
