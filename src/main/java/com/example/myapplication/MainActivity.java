e3df620cdc8e838b8a4e95fd2f4766e645514afe
betkibet <kibetnamanga@gmail.com>
2019-07-18 10:38:46 +0300

    updated

--- src/main/java/com/example/myapplication/MainActivity.java
+++ src/main/java/com/example/myapplication/MainActivity.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.example.myapplication;
 
 import android.content.Intent;
@@ -55,61 +54,4 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 }
-=======
-package com.example.myapplication;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.Button;
-
-public class MainActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        Button aboutAlc = findViewById(R.id.button_aboutalc);
-        Button myProfile = findViewById(R.id.button_myprofile);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-    public void myProfile(View v){
-        Intent intent = new Intent(this, MyProfile.class);
-        startActivity(intent);
-    }
-    public void aboutAlc(View v){
-        Intent intent = new Intent(this, AboutAlc.class);
-        startActivity(intent);
-    }
-}
->>>>>>> project commit
