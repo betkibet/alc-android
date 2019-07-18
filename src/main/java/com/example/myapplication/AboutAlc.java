ac849acd7605b6353eae85efa72e58135e78383b
unknown <namangadaniel@gmail.com>
2019-07-18 09:34:01 +0300

    updated some files

--- src/main/java/com/example/myapplication/AboutAlc.java
+++ src/main/java/com/example/myapplication/AboutAlc.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 package com.example.myapplication;
 
 import android.content.Intent;
@@ -26,3 +27,33 @@ public class AboutAlc extends AppCompatActivity {
     }
 
 }
+=======
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.webkit.WebView;
+
+public class AboutAlc extends AppCompatActivity {
+
+    private WebView mWebview;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_alc);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        mWebview = (WebView)findViewById(R.id.webview);
+        mWebview.loadUrl("https://andela.com/alc/");
+
+
+    }
+
+}
+>>>>>>> project commit
--- src/main/java/com/example/myapplication/MyProfile.java
+++ src/main/java/com/example/myapplication/MyProfile.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 package com.example.myapplication;
 
 import android.os.Bundle;
@@ -18,3 +19,25 @@ public class MyProfile extends AppCompatActivity {
     }
 
 }
+=======
+package com.example.myapplication;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class MyProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_profile);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+}
+>>>>>>> project commit
